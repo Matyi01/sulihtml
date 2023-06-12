505c6e5f9736a5dccdb69199fd5146a8a4dcc92d
Matyi01 <holczer.matyi.21@ady-nagyatad.hu>
2023-06-12 17:48:27 +0200

    agazati2022

--- agazati_alapvizsga/2022/3.Python/2. feladat.py
+++ agazati_alapvizsga/2022/3.Python/2. feladat.py
@@ -1,2 +1,27 @@
 import haromszog
-pelda = haromszog.haromszog()
+
+def bekeres():
+    haromszogek = []
+    for i in range(3):
+        print(f"{i+1}. adatsor")
+        temp = haromszog.haromszog()
+        haromszogek.append(temp)
+    for e in haromszogek:
+        print(f"\ta={e[0]}, b={e[1]}, c={e[2]}")
+    return haromszogek
+
+def haromszoge(oldalak):
+    a = oldalak[0]
+    b = oldalak[1]
+    c = oldalak[2]
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    else:
+        return False
+
+adatok = bekeres()
+for i,e in enumerate(adatok):
+    if haromszoge(e) == True:
+        print(f"{i+1}. számsor lehet háromszög")
+    else:
+        print(f"{i+1}. számsor nem lehet háromszög")
--- agazati_alapvizsga/2022/3.Python/haromszog.py
+++ agazati_alapvizsga/2022/3.Python/haromszog.py
@@ -1,5 +1,20 @@
 def haromszog():
-    a = int(input("Kérem a(z) 1. egy egész számot: "))
-    b = int(input("Kérem a(z) 2. egy egész számot: "))
-    c = int(input("Kérem a(z) 3. egy egész számot: "))
+    a = ""
+    while a == "":
+        try:
+            a = int(input("Kérem a(z) 1. egy egész számot: "))
+        except:
+            print("Ez nem egész szám!")
+    b = ""
+    while b == "":
+        try:
+            b = int(input("Kérem a(z) 2. egy egész számot: "))
+        except:
+            print("Ez nem egész szám!")
+    c = ""
+    while c == "":
+        try:
+            c = int(input("Kérem a(z) 3. egy egész számot: "))
+        except:
+            print("Ez nem egész szám!")
     return [a, b, c]
